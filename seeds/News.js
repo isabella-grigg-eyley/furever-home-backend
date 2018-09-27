9ef77a36b4cd138de3f711c8000cfd5ee48833de
lexi-luo <lexiluo@gmail.com>
2018-09-27 19:22:22 +1200

    db and seeds tidy up

--- seeds/News.js
+++ seeds/News.js
@@ -3,7 +3,7 @@ exports.seed = function(knex, Promise) {
     return knex('News').del()
       .then(function () {
         return knex('News').insert([
-          {id: 1, organisation: 'SPCA', headline: 'Grumpy Cat would like to be known as Crabby Cat ', content: 'lkjlahkglahg aerhto akjalgdfkgj alerit lh', photo: 'https://pbs.twimg.com/profile_images/948294484596375552/RyGNqDEM_400x400.jpg'},
+          {id: 1, organisation: 'SPCA', headline: 'Grumpy Cat would like to be known as Crabby Cat ', content: 'lkjlahkglahg aerhto akjalgdfkgj alerit lh', image: 'https://pbs.twimg.com/profile_images/948294484596375552/RyGNqDEM_400x400.jpg'},
         ]);
       });
   };
