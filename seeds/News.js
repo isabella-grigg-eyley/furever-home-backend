0671603ce05b61b0f6edaf0db978e9bbfd3b5f92
lexi-luo <lexiluo@gmail.com>
2018-09-29 14:37:34 +1200

    to add to

--- seeds/News.js
+++ seeds/News.js
@@ -4,6 +4,10 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       return knex('News').insert([
         { id: 1, organisation: 'SPCA', headline: 'Grumpy Cat would like to be known as Crabby Cat ', content: 'lkjlahkglahg aerhto akjalgdfkgj alerit lh', image: 'https://pbs.twimg.com/profile_images/948294484596375552/RyGNqDEM_400x400.jpg' },
+        { id: 2, organisation: 'SPCA', headline: 'Grumpy Cat would like to be known as Crabby Cat ', content: 'lkjlahkglahg aerhto akjalgdfkgj alerit lh', image: 'https://pbs.twimg.com/profile_images/948294484596375552/RyGNqDEM_400x400.jpg' },
+        { id: 3, organisation: 'SPCA', headline: 'Grumpy Cat would like to be known as Crabby Cat ', content: 'lkjlahkglahg aerhto akjalgdfkgj alerit lh', image: 'https://pbs.twimg.com/profile_images/948294484596375552/RyGNqDEM_400x400.jpg' },
+        { id: 4, organisation: 'SPCA', headline: 'Grumpy Cat would like to be known as Crabby Cat ', content: 'lkjlahkglahg aerhto akjalgdfkgj alerit lh', image: 'https://pbs.twimg.com/profile_images/948294484596375552/RyGNqDEM_400x400.jpg' },
+        { id: 5, organisation: 'SPCA', headline: 'Grumpy Cat would like to be known as Crabby Cat ', content: 'lkjlahkglahg aerhto akjalgdfkgj alerit lh', image: 'https://pbs.twimg.com/profile_images/948294484596375552/RyGNqDEM_400x400.jpg' }
       ]);
     });
 };
--- seeds/Users.js
+++ seeds/Users.js
@@ -1,12 +1,13 @@
 
-exports.seed = function(knex, Promise) {
- 
+exports.seed = function (knex, Promise) {
+
   return knex('Users').del()
     .then(function () {
-    
+
       return knex('Users').insert([
-        {id: 1, username: '123', password: '123', email: '123@123.com', coins: '100', logoutTime: "1538001217263"},
-        {id: 2, username: '456', password: '456', email: '456@456.com', coins: '1000', logoutTime: "1538001217245"}
+        { id: 1, username: 'harri-sun', password: '111', email: '123@123.com', coins: '100', logoutTime: "1538001217263" },
+        { id: 2, username: 'brucegoose', password: '222', email: '456@456.com', coins: '1000', logoutTime: "1538001217245" },
+        { id: 2, username: 'badboy69', password: '333', email: '789@789.com', coins: '1000', logoutTime: "1538001217245" }
       ]);
     });
 };
