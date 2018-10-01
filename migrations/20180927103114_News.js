c8aaf3c593c2f7552965bb412bd7e22de8769bc0
isabella-grigg-eyley <isabella.grigg.eyley@gmail.com>
2018-10-01 15:38:19 +1300

    changed db string fields to text

--- migrations/20180927103114_News.js
+++ migrations/20180927103114_News.js
@@ -1,15 +1,15 @@
 
-exports.up = function(knex, Promise) {
-    return knex.schema.createTable('News', function (table){
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('News', function (table) {
         table.increments().primary()
         table.string('organisation')
         table.string('headline')
-        table.string('content')
+        table.text('content', 50000)
         table.string('image')
     })
-  
+
 }
 
-exports.down = function(knex, Promise) {
+exports.down = function (knex, Promise) {
     return knex.schema.dropTable('News')
 };
