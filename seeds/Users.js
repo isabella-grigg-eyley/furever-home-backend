70c49b30fae320062728755494468c632c1a69d0
isabella-grigg-eyley <isabella.grigg.eyley@gmail.com>
2018-10-01 17:20:33 +1300

    fixed seed again

--- seeds/Users.js
+++ seeds/Users.js
@@ -5,9 +5,9 @@ exports.seed = function (knex, Promise) {
     .then(function () {
 
       return knex('Users').insert([
-        { id: 1, username: 'harri-san', password: '111', email: '123@123.com', coins: '100', logoutTime: "1538001217263" },
-        { id: 2, username: 'brucegoose', password: '222', email: '456@456.com', coins: '1000', logoutTime: "1538001217245" },
-        { id: 3, username: 'badboy69', password: '333', email: '789@789.com', coins: '1000', logoutTime: "1538001217245" }
+        { id: 1, username: 'harri-san', password: '111', email: '123@123.com', coins: '100', logoutTime: 1538001217263 },
+        { id: 2, username: 'brucegoose', password: '222', email: '456@456.com', coins: '1000', logoutTime: 1538001217245 },
+        { id: 3, username: 'badboy69', password: '333', email: '789@789.com', coins: '1000', logoutTime: 1538001217245 }
       ]);
     });
 };
