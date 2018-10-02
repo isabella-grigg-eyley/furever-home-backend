e788fc9297dba789ecd182ff5d35508ff72010fe
lexi-luo <lexiluo@gmail.com>
2018-10-03 09:05:06 +1300

    updated image links

--- seeds/Cosmetics.js
+++ seeds/Cosmetics.js
@@ -1,11 +1,13 @@
 
-exports.seed = function(knex, Promise) {
-    return knex('Cosmetics').del()
-      .then(function () {
-        return knex('Cosmetics').insert([
-          {id: '1', name: 'tophat', image: 'https://www.villagehatshop.com/photos/product/standard/4511390S423245/all/abraham-lincoln-wool-felt-top-hat-made-to-order.jpg', price: '100'},
-          {id: '2', name: 'bow', image: '', price: '200'}
-        ]);
-      });
-  };
-  
+exports.seed = function (knex, Promise) {
+  return knex('Cosmetics').del()
+    .then(function () {
+      return knex('Cosmetics').insert([
+        { id: '1', name: 'Bowler Hat', image: '/images/cosmetics/bowler-hat-512-png', price: '100' },
+        { id: '2', name: 'Glasses', image: '/images/cosmetics/glasses-2-png', price: '200' },
+        { id: '3', name: 'Crown', image: '/images/cosmetics/crown-2-512-png', price: '500' },
+        { id: '4', name: 'Tie', image: '/images/cosmetics/tie-512-png', price: '300' },
+        { id: '5', name: 'Moustache', image: '/images/cosmetics/mustache-2-png', price: '700' }
+      ]);
+    });
+};
