80acc7be630a4c0469727d1508430eb5ad8d00d7
isabella-grigg-eyley <isabella.grigg.eyley@gmail.com>
2018-10-02 19:12:03 +1300

    reordered seeds

--- seeds/Animals.js
+++ seeds/Animals.js
@@ -6,9 +6,9 @@ exports.seed = function (knex, Promise) {
 
       return knex('Animals').insert([
         { id: 1, name: 'Chippy', species: 'dog', disposition: 'sassy' },
-        { id: 2, name: 'Star', species: 'dog', disposition: 'regal' },
-        { id: 3, name: 'Twinkie', species: 'dog', disposition: 'happy' },
-        { id: 4, name: 'Donut', species: 'cat', disposition: 'grumpy' },
+        { id: 2, name: 'Donut', species: 'cat', disposition: 'grumpy' },
+        { id: 3, name: 'Star', species: 'dog', disposition: 'regal' },
+        { id: 4, name: 'Twinkie', species: 'dog', disposition: 'happy' },
         { id: 5, name: 'Carlos', species: 'rabbit', disposition: 'sleepy' },
       ]);
     });
